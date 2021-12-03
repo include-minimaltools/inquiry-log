ca591fd91b4a5172929bcf578acafcb606164578
Gabriel <gabrielortizamador@gmail.com>
2021-12-03 01:04:37 -0600

    Conexión del calendario con backend

--- ClientApp/src/views/events/index.js
+++ ClientApp/src/views/events/index.js
@@ -1,8 +1,36 @@
-import { Calendar, Row } from "antd";
-import React from "react";
+import { Badge, Calendar, Row } from "antd";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { InquiryService } from "../../service";
 
 function Events() {
-  return <Calendar fullscreen={true} />;
+  const [inquiries, setInquiries] = useState([]);
+
+  useEffect(() => {
+    async function getInquiries() {
+      setInquiries(await InquiryService.getInquiries());
+    }
+    getInquiries();
+  }, []);
+
+  const dateCellRender = (value) => {
+    const listData = inquiries.filter((item) => {
+      console.log(new moment(item.created_On).format("YYYY-MM-DD") === value.format("YYYY-MM-DD"));
+      return new moment(item.created_On).format("YYYY-MM-DD") === value.format("YYYY-MM-DD");
+    });
+
+    return (
+      <div className="events">
+        {listData.map((item) => (
+          <div key={item?.subject}>
+            <Badge status={item?.status === 'Pendiente' ? 'warning' : 'success'} text={item?.subject} />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return <Calendar fullscreen={true} dateCellRender={dateCellRender} />;
 }
 
 export default Events;
