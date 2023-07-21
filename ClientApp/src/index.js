bb81426bfeb028a8763575b52594e116d9da1d6e
include-minimaltools <gabrielortizamador@gmail.com>
2023-07-21 02:37:36 -0600

    Fixed deprecated dependences

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "antd/dist/antd.css";
+import "antd/dist/antd.variable.css";
+import { ConfigProvider } from "antd";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 
+ConfigProvider.config({
+  theme: {
+    primaryColor: "#003782",
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <ConfigProvider>
     <Router>
       <Routes />
     </Router>
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
